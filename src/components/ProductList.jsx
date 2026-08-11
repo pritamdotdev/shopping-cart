e09ad449abe4a12bcabee50808c88cb2e25dae7e
Pritam Paul <pritampaul5530@gmail.com>
2026-08-11 23:38:36 +0530

    Updated features

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,13 @@
+import { useProducts } from "../context/ProductContext";
 import ProductCard from "./ProductCard";
 
-const ProductList = ({ products }) => {
+const ProductList = () => {
+  const { products, loading, error } = useProducts();
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      {loading && <p>Loading...</p>}
+      {error && <div className="error">❌ {error}</div>}
+
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
